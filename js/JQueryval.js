95c29e5a8d42c66132104b8b654185cc70fc6258
Alonso Silva <re.silvas@duocuc.cl>
2023-04-19 15:39:36 -0400

    Se creo la primera validacion

--- js/JQueryval.js
+++ js/JQueryval.js
@@ -1,8 +1,10 @@
 $(function(){ 
     $("#sendB").click(function() {
         $(".text-danger").remove(); //esto es para volver a ocultar las etiquetas de validacionS
-        $(".form-control").each(function(indice, elemento) {
-            console.log('El elemento con el índice '+indice+' contiene '+$(elemento).text());
+        $(".form-control").each(function(index, element) {
+            if(index>= 1  && index<=3){
+                console.log($(element).val());
+            }
         });
     });
 
