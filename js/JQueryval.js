c7add0297a21635cadbb2d219f1954b58d78a048
Alonso Silva <re.silvas@duocuc.cl>
2023-04-19 15:22:15 -0400

    Recorrido inicial del array de formularios

--- js/JQueryval.js
+++ js/JQueryval.js
@@ -1,6 +1,9 @@
 $(function(){ 
     $("#sendB").click(function() {
         $(".text-danger").remove(); //esto es para volver a ocultar las etiquetas de validacionS
+        $(".form-control").each(function(indice, elemento) {
+            console.log('El elemento con el índice '+indice+' contiene '+$(elemento).text());
+        });
     });
-    
+
 });
